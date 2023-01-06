259dfac2365b2f47e0775891f59e4aa7a458fb88
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-06 20:18:37 +0500

    asd

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -46,65 +46,68 @@ const Landing = () => {
   }, []);
 
   return (
-    <div className="container landing-wrapp">
-      <img src="/BACKGROUND LAZERS.png" alt="" />
+    <>
       <canvas className="code"></canvas>
-      {/* <img src="/code.png" className="code" alt="" /> */}
-      <div className="landing-div">
-        <img src="/main-ico.png" className="logo" alt="" />
-        <div className="main-square">
-          <h3>
-            <span>We are</span> trend hackers
-          </h3>
-          <h4>Trending Service</h4>
-          <p>
-            We pride ourselves on being at the cutting edge of each and every
-            guerilla marketing campaign technique implementable to baffle and
-            manipulate algorithms on all the leading crypt and social platforms.
-          </p>
-          <p>
-            This allows us to manipulate the invisible but very real boundaries
-            set by platform owners to Limit your visibility.{" "}
-          </p>
-          <h5>
-            <span>WE ARE THE</span> DIRTY SECRET{" "}
-            <span>PEOPLE WHISPER ABOUT</span>
+      <div className="container landing-wrapp">
+        <img src="/BACKGROUND LAZERS.png" alt="" />
+
+        <div className="landing-div">
+          <img src="/main-ico.png" className="logo" alt="" />
+          <div className="main-square">
+            <h3>
+              <span>We are</span> trend hackers
+            </h3>
+            <h4>Trending Service</h4>
+            <p>
+              We pride ourselves on being at the cutting edge of each and every
+              guerilla marketing campaign technique implementable to baffle and
+              manipulate algorithms on all the leading crypt and social
+              platforms.
+            </p>
+            <p>
+              This allows us to manipulate the invisible but very real
+              boundaries set by platform owners to Limit your visibility.{" "}
+            </p>
+            <h5>
+              <span>WE ARE THE</span> DIRTY SECRET{" "}
+              <span>PEOPLE WHISPER ABOUT</span>
+            </h5>
+            <h6>
+              {" "}
+              <span>WE ARE THE</span> RUMOUR{" "}
+              <span>YOU HEARD AND DIDN'T BELIEVE </span>
+            </h6>
+            <p>
+              We ARE used by some of the most well known crypto projects and
+              marketing agencies across the world , as they outsource this very
+              tech heavy part of the job.{" "}
+            </p>
+            <p>
+              Best of all, the majority of what we do provides trackable and
+              measurable results across crypto and social media platforms.{" "}
+            </p>
+            <h5>
+              <span>
+                ALL OF WHAT WE DO IS DESIGNED TO CREATE vlsosii_rry AND ONE
+                THING IS FOR CERTAIN ,{" "}
+              </span>
+            </h5>
+            <h5>
+              <span>
+                BEING SPAWNED IN THE DEPTHS OF THE BSC DEGEN COMMUNITY <br />{" "}
+                WHERE BUDGET'S ARE LOW AND EXPECTATIONS ARE HIGH.{" "}
+              </span>
+            </h5>
+            <p className="white-text">
+              EMAIL INFO@TREND-HACKERS COM FOR INQUIRIES{" "}
+            </p>
+          </div>
+          <h5 className="end-text">
+            THOSE WHO ARE STILL IN BUISINESS CERTAINLY LEARNED SOME THINGS{" "}
           </h5>
-          <h6>
-            {" "}
-            <span>WE ARE THE</span> RUMOUR{" "}
-            <span>YOU HEARD AND DIDN'T BELIEVE </span>
-          </h6>
-          <p>
-            We ARE used by some of the most well known crypto projects and
-            marketing agencies across the world , as they outsource this very
-            tech heavy part of the job.{" "}
-          </p>
-          <p>
-            Best of all, the majority of what we do provides trackable and
-            measurable results across crypto and social media platforms.{" "}
-          </p>
-          <h5>
-            <span>
-              ALL OF WHAT WE DO IS DESIGNED TO CREATE vlsosii_rry AND ONE THING
-              IS FOR CERTAIN ,{" "}
-            </span>
-          </h5>
-          <h5>
-            <span>
-              BEING SPAWNED IN THE DEPTHS OF THE BSC DEGEN COMMUNITY <br />{" "}
-              WHERE BUDGET'S ARE LOW AND EXPECTATIONS ARE HIGH.{" "}
-            </span>
-          </h5>
-          <p className="white-text">
-            EMAIL INFO@TREND-HACKERS COM FOR INQUIRIES{" "}
-          </p>
         </div>
-        <h5 className="end-text">
-          THOSE WHO ARE STILL IN BUISINESS CERTAINLY LEARNED SOME THINGS{" "}
-        </h5>
       </div>
-    </div>
+    </>
   );
 };
 
