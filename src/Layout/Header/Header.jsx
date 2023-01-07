f03ad97ae9731b0452ac7ed7e19a9d59b598b9e5
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-07 11:38:47 +0500

    asd

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -4,23 +4,34 @@ import "./Header.css";
 const Header = () => {
   const [headerToggle, setHeaderToggle] = useState(false);
   return (
-    <div className="container header-wrapp">
-      {headerToggle && <div className="overlay"></div>}
-      <header>
-        <img
-          onClick={() => setHeaderToggle((prev) => !prev)}
-          src="/STACK LINE.png"
-          className="header-toggle"
-          alt=""
-        />
-        <nav className={headerToggle ? "active-side" : ""}>
-          <a href="#">Summary</a>
-          <a href="#">Contact</a>
-          <a href="#">Links</a>
-        </nav>
-        <img src="/ico.png" alt="" />
-      </header>
-    </div>
+    <>
+      {headerToggle && (
+        <div onClick={() => setHeaderToggle(false)} className="overlay"></div>
+      )}
+      <div className="container header-wrapp">
+        <header>
+          <img
+            onClick={() => setHeaderToggle((prev) => !prev)}
+            src="/STACK LINE.png"
+            className="header-toggle"
+            alt=""
+          />
+          <nav className={headerToggle ? "active-side" : ""}>
+            <a onClick={() => setHeaderToggle(false)} href="#">
+              Summary
+            </a>
+            <a onClick={() => setHeaderToggle(false)} href="#">
+              Contact
+            </a>
+            <a onClick={() => setHeaderToggle(false)} href="#">
+              Links
+            </a>
+          </nav>
+
+          <img src="/ico.png" alt="" />
+        </header>
+      </div>{" "}
+    </>
   );
 };
 
